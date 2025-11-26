bc45e78db378a13b205d2345a25d308b19b33371
juanpabril <juanpa.abrilr@gmail.com>
2025-11-26 14:33:48 -0700

    Update solucion.py (con cadenas largas)

--- solucion.py
+++ solucion.py
@@ -7,12 +7,14 @@ def reloj_arena(m: int, s: str) -> str:
     
     # TODO: implementar la lógica para generar el reloj de arena en ASCII
     for i in range(1,m+1):
-        print((i-1)*" ", end="")
+        print((i-1)*(" "*len(s)), end="")
         print((2*(m-i+1)-1)*s)
 
     for i in range(1,m):
         ind = (m-i)
-        print((ind-1)*" ", end="")
+        print((ind-1)*(" "*len(s)), end="")
         print((2*(m-ind+1)-1)*s)
+        
+        
         
         
